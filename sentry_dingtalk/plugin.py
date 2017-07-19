2643787952c6ecb9419d36b12b36878b0d755d84
guoyong yi <451417726@qq.com>
2017-07-20 00:10:28 +0800

    update description

--- sentry_dingtalk/plugin.py
+++ sentry_dingtalk/plugin.py
@@ -2,7 +2,7 @@
 sentry_dingtalk.models
 ~~~~~~~~~~~~~~~~~~~~~
 
-:copyright: (c) 2017 by Guoyong YI, see AUTHORS for more details.
+:copyright: (c) 2017 by Guoyong yi, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
 
